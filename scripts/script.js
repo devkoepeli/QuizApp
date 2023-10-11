fab76321d7fe5183380aa8e542db9eccd88a8ddc
devkoepeli <devrim_kopeli@hotmail.com>
2023-10-11 22:39:13 +0300

    css for solution of question

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,17 @@ let questions = [
         "answer_2": "Lady Gaga",
         "answer_3": "Tim-Berners-Lee",
         "answer_4": "Justin TImberlake",
-        "right_answer": 3
+        "right_answer": 3,
+        "question_number": 1
     },
     {    
         "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &1t;frameset&gt;",
-        "answer_2": "&It;iframe&gt;",
-        "answer_3": "&lt; frame&gt;",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, und &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt;frame&gt;",
         "answer_4": "&lt;frameset&gt;",
-        "right_answer": 2
+        "right_answer": 2,
+        "question_number": 2
     },
     {
         "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
@@ -21,15 +23,17 @@ let questions = [
         "answer_2": "Container",
         "answer_3": "Ein Link",
         "answer_4": "Kursiv",
-        "right_answer": 3
+        "right_answer": 3,
+        "question_number": 3
     },
     {
         "question": "Wie stellt man Text am BESTEN fett dar?",
         "answer_1": "&lt;strong&gt;",
-        "answer 2": "CSS nutzen",
+        "answer_2": "CSS nutzen",
         "answer_3": "&lt;bold&gt",
         "answer_4": "&lt;b&gt;",
-        "right_answer": 1
+        "right_answer": 1,
+        "question_number": 4
     },
     {
         "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
@@ -37,7 +41,8 @@ let questions = [
         "answer_2": "max",
         "answer_3": "from",
         "answer_4": "spellcheck",
-        "right answer": 1
+        "right answer": 1,
+        "question_number": 5
     },
     {
         "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem Attribut title aus?",
@@ -45,7 +50,8 @@ let questions = [
         "answer_2": "a > title {••",
         "answer_3": "a.title {..•)",
         "answer_4": "a-title {*",
-        "right_answer": 1
+        "right_answer": 1,
+        "question_number": 6
     },
     {
         "question": "Wie definiert man in JavaScript eine variable?",
@@ -53,7 +59,8 @@ let questions = [
         "answer_2": "100 = let rate;",
         "answer_3": "rate = 100;",
         "answer_4": "let rate = 100;",
-        "right_answer": 4
+        "right_answer": 4,
+        "question_number": 7
     }
 ];
 
@@ -63,10 +70,11 @@ function showQuestion(i) {
     cardContainer.innerHTML = '';
     
     cardContainer.innerHTML = templateQA(i);
+    highlightLanguage(i);
 }
 
 
-function showStart(i) {
+function showPrevious(i) {
     let cardContainer = document.getElementById('card');
 
     if (i === 0) {
@@ -97,11 +105,46 @@ function showFinish() {
 }
 
 
+function checkValidity(i) {
+    let rightAnswer = questions[i]['right_answer'];
+    let answer = document.getElementById('option0');
+    if (rightAnswer === answer) {
+        answer.classList.add('bg-green');
+    } else {
+        answer.classList.add('bg-red');
+    }
+}
+
+
+function highlightLanguage(i) {
+    let indexOfQuestions = questions.indexOf(questions[i]);
+    let elementHTML = document.getElementById('html');
+    let elementJS = document.getElementById('JS');
+    let elementCSS = document.getElementById('CSS');
+    if ([0,1,2,3,4].includes(indexOfQuestions)) {
+        elementHTML.classList.add('font-white');
+    } else {
+        elementHTML.classList.remove('font-white');
+            if ([6].includes(indexOfQuestions)) {
+                elementJS.classList.add('font-white');
+            } else {
+                elementJS.classList.remove('font-white');
+                if (indexOfQuestions === 5) {
+                    elementCSS.classList.add('font-white')
+                } else {
+                    elementCSS.classList.remove('font-white')
+                }
+            } 
+                
+    }
+}
+
+
 function templateQA(i) {
     return `
         <h5 class="card-title mt-4">${questions[i]['question']}</h5>
         <div class="card-answers">
-            <div class="card mb-3 card-answer">
+            <div onclick="checkValidity(${i})" id="option0" class="card mb-3 card-answer">
                 <div class="card-option">
                     A
                 </div>
@@ -136,8 +179,8 @@ function templateQA(i) {
         </div>
         
         <div class="question-footer mt-2">
-            <button onclick="showStart(${i})" type="button" class="btn btn-primary">Zurück</button>
-            <span><strong>1</strong> von <strong>7</strong> Fragen</span>
+            <button onclick="showPrevious(${i})" type="button" class="btn btn-primary">Zurück</button>
+            <span><strong>${questions[i]['question_number']}</strong> von <strong>${questions.length}</strong> Fragen</span>
             <button onclick="showNextQuestion(${i})" type="button" class="btn btn-primary">Nächste Frage</button>
         </div>
     `;
@@ -157,6 +200,7 @@ function templateStart() {
         </div>
     `;
 }
+
 
 function templateFinish() {
     return /*html*/ `
